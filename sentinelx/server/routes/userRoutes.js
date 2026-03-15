459debf45a54f2ca23ba92048cccd434751a8244
Nishanth <nishanth7929@gmail.com>
2026-03-15 18:05:45 +0530

    Remove nested sentinelx folder

--- sentinelx/server/routes/userRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const { upsertUser, updateUserLocation } = require('../controllers/usersController');
-
-const router = express.Router();
-
-router.post('/users/register', upsertUser);
-router.patch('/users/location', updateUserLocation);
-
-module.exports = router;
